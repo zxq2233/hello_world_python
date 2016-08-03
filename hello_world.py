353c2960915926cced1e8f0eccab717929d598e3
zxq2233 <531373473@qq.com>
2016-08-03 19:53:21 +0800

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,13 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello_world():
-    return "Hello World!"
-
-@app.route("/hello/<name>")
-def hello_person(name):
-    return "Hello {}!".format(name.title())
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+@app.route('/')
+def source():
+ html = 'Hello World!'
+ return html
